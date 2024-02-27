4fcfe99a9c6036a4b01ca9a082a5c83dddedc8b5
Carl Molina <carlandrew.molina@gmail.com>
2024-02-27 15:24:08 -0800

    first draft of revString fn

--- recursion/rev-string/revString.ts
+++ recursion/rev-string/revString.ts
@@ -1,7 +1,21 @@
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str: string): string {
-  return "x";
+
+  let out = str[str.length - 1];
+
+  // base case
+  if (str.length === 0) {
+    return str;
+  }
+
+  // general case
+  const char = revString(str.slice(0, -1));
+  console.log('This is char after revString:', char);
+
+  console.log('This is out before inserting char:', out);
+  return out += char;
+
 }
 
 export { revString };
