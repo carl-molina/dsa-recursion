68f7c3892e75684d0d2dd611701a7fb199f5f559
Brandie Lucano <blucano.dev@gmail.com>
2024-02-27 12:33:51 -0800

    everyOther working and test passing.

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -1,7 +1,12 @@
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+
+  //Base case: string is empty
+  if(str === '') return '';
+
+  //h //e\\
+  return str.slice(0, 1) + everyOther(str.slice(2));
 }
 
 export { everyOther };
