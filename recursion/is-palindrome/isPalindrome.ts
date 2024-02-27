0781f36cad46208fef84cad5c25961c90ad18cc6
Carl Molina <carlandrew.molina@gmail.com>
2024-02-27 15:06:08 -0800

    initial draft of isPalindrome fn

--- recursion/is-palindrome/isPalindrome.ts
+++ recursion/is-palindrome/isPalindrome.ts
@@ -1,7 +1,17 @@
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str: string): boolean {
-  return true;
+
+  // base case
+  if (str.length === 0) return true;
+  if (str.length === 1) return true;
+
+  // general case
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.slice(1, -1));
+  } else {
+    return false;
+  }
 }
 
 export { isPalindrome };
