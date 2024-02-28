fea23b1af96ea2bf8a6a38a7744e768e50235d76
Carl Molina <carlandrew.molina@gmail.com>
2024-02-27 16:37:26 -0800

    passing draft of binarySearchIndex w/ passing tests

--- recursion/binary-search-index/binarySearchIndex.ts
+++ recursion/binary-search-index/binarySearchIndex.ts
@@ -2,11 +2,31 @@
  * return the index of that value (or -1 if val is not present). */
 
 function binarySearchIndex(
-    arr: number[],
-    val: number,
-    left = 0,
-    right = arr.length): number {
-  return 42;
+  arr: number[],
+  val: number,
+  left = 0,
+  right = arr.length): number {
+
+  // base case
+  if (left >= right) {
+    return -1;
+  }
+
+  // general case
+  const mid = Math.floor((left + right - 1) / 2);
+  const guess = arr[mid];
+
+  if (guess === val) {
+    return mid;
+  } else {
+    if (val < guess) {
+      right = mid - 1;
+    } else if (val > guess) {
+      left = mid + 1;
+    }
+  }
+
+  return binarySearchIndex(arr, val, left, right);
 }
 
 export { binarySearchIndex };
