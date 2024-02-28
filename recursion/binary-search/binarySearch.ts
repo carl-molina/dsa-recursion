8fca86087f8f0ff2ba0d73b5076f9bb275891229
Carl Molina <carlandrew.molina@gmail.com>
2024-02-27 16:19:04 -0800

    working draft of binarySearch fn w/ passing tests

--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -2,7 +2,25 @@
  * return true if val is in array, false if not present). */
 
 function binarySearch(arr: number[], val: number, left = 0, right = arr.length): boolean {
-  return false;
+
+  if (left >= right) {
+    return false;
+  }
+
+  const mid = Math.floor((left + right - 1) / 2);
+
+  const guess = arr[mid];
+  if (guess === val) {
+    return true;
+  } else {
+    if (val < guess) {
+      right = mid - 1;
+    } else if (val > guess) {
+      left = mid + 1;
+    }
+  }
+
+  return binarySearch(arr, val, left, right);
 }
 
 export { binarySearch };
